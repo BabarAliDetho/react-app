7bef469e3591530b3dc34d80cb138e10a469ff8b
Babar Detho <kingbabarali@gmail.com>
2020-06-10 03:24:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
       <h1> Hello World from Mr.Babar-Detho </h1>
-      <h2><href>sordid-account.surge.sh</href></h2>
-      <h3><href>babardetho.surge.sh</href></h3>
-      <h4>Comming Soon</h4>
+      <h2>Comming Soon</h2>
   );
 }
 
