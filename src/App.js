43d9db12d078462c9e776956e18e15d30d420ef3
Babar Detho <kingbabarali@gmail.com>
2020-06-13 23:24:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
       <div><h1> Hello World from Babar-Detho </h1>
-      <h2>Comming Soon</h2></div>
+      <h1>Comming Soon ASAP AVOCADO</h1></div>
   );
 }
 
