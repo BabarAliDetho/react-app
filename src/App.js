f0a1baa122edc35335751846df138195a4d9f915
Babar Detho <kingbabarali@gmail.com>
2020-06-14 17:19:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
-function App() {
-  return (
-      <div><h1> Hello World from Babar-Detho </h1>
-      <h1>Comming Soon ASAP AVOCADO</h1></div>
-  );
-}
+import dinner from './dinner.js';
 
+function App () {
+    return (
+        <div classname="App">
+            {/* <Dinner></Dinner> */}
+        <dinner />
+        </div>
+    );
+  }
+  
 export default App;
