5879af2a7acf3ffea9d37a4f31300426490f4927
Babar Detho <kingbabarali@gmail.com>
2020-06-14 18:56:27 +0500

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -4,7 +4,9 @@ import React from 'react';
 function Dinner (props) {
     return (
         <div>
-            <h1>today{props.dishName}</h1>
+            <h1>Github { props.dishName}</h1>
+            <h1>still $0 earn</h1>
+            <h1>need of fast track just follow the steps then understand concept of happenings, interested in actions, not in theory</h1>
         </div>
     )
   }
