415941e92013c70327e0ce846fe7770b13526d80
Babar Ali <kingbabarali@gmail.com>
2020-07-01 09:49:04 -0700

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -6,7 +6,7 @@ function Dinner (props) {
         <div>
             <h1>Github { props.dishName}</h1>
             <h1>still $0 earn</h1>
-            <h1>need of fast track just follow the steps then understand concept of happenings, interested in actions, not in theory</h1>
+            <h1>need of fast track just follow the steps then understand concept of happenings.</h1>
         </div>
     )
   }
