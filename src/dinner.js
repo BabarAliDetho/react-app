1054e35a053cffd39b2fc8d1f919b88b16e2c9aa
HBKD <kingbabarali@gmail.com>
2020-09-04 13:21:26 -0700

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -5,8 +5,7 @@ function Dinner (props) {
     return (
         <div>
             <h1>Github { props.dishName}</h1>
-            <h1>still $0 earn</h1>
-            <h1>need of fast track just follow the steps then understand concept of happenings.</h1>
+            <h1>Email Marketing</h1>
         </div>
     )
   }
