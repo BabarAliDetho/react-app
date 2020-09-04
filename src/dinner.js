de96324a5ff73059c2442db02f68a703cdcdf354
HBKD <kingbabarali@gmail.com>
2020-09-04 14:43:54 -0700

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -5,8 +5,8 @@ function Dinner (props) {
     return (
         <div>
             <h1>Email Marketing{ props.dishName}</h1>
-            <Image>https://scontent.fkhi4-2.fna.fbcdn.net/v/t1.0-9/38992248_858208874377252_1739280795022917632_n.jpg?_nc_cat=101&_nc_sid=174925&_nc_eui2=AeFFNdyyBgb7wxKHLynsEgzhqBRzcNn6QS-oFHNw2fpBL2iK9bs4E6TzaZRlp5wp10z9TDdcGD5mLmY4Ig4nfJEk&_nc_ohc=CN5uyMFwioUAX-tjzaj&_nc_ht=scontent.fkhi4-2.fna&oh=f2409acc5c408cbfcbda2608b88f7c98&oe=5F78D721
-            </Image>
+            <link>https://scontent.fkhi4-2.fna.fbcdn.net/v/t1.0-9/38992248_858208874377252_1739280795022917632_n.jpg?_nc_cat=101&_nc_sid=174925&_nc_eui2=AeFFNdyyBgb7wxKHLynsEgzhqBRzcNn6QS-oFHNw2fpBL2iK9bs4E6TzaZRlp5wp10z9TDdcGD5mLmY4Ig4nfJEk&_nc_ohc=CN5uyMFwioUAX-tjzaj&_nc_ht=scontent.fkhi4-2.fna&oh=f2409acc5c408cbfcbda2608b88f7c98&oe=5F78D721
+</link>            
         </div>
     )
   }
