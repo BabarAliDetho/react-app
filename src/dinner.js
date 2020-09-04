ca78be4499f41ea6201e6aab13cb66398da97c1c
HBKD <kingbabarali@gmail.com>
2020-09-04 13:55:44 -0700

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -4,8 +4,7 @@ import React from 'react';
 function Dinner (props) {
     return (
         <div>
-            <h1>Github { props.dishName}</h1>
-            <h1>Email Marketing</h1>
+            <h1>Email Marketing{ props.dishName}</h1>
         </div>
     )
   }
